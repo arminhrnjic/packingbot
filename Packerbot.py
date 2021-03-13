71355716601b3a0f6b7c82f8416f7e1f61d4b096
Armin Hrnjić <arminh@ki.si>
2021-03-13 13:59:49 +0100

    Create Packerbot.py

---
+++ Packerbot.py
@@ -0,0 +1,28 @@
+import discord
+
+client=discord.Client()
+
+prefix='?'
+TOKEN='ODIwMjQwNTk3OTMwOTk5ODE4.YEySlQ.lojwqI7_oefr-IkOXdsA70OD-Jc'
+
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.author != client.user:
+        print(message.author.id, message.author)
+    
+    if message.content.startswith(prefix+'pack'):
+        await message.channel.send(str(message.author) + ' needs a pack @everyone')
+        await message.add_reaction('\U0001F199')
+        await message.add_reaction('\U00002705')
+    if message.content.startswith(prefix+'help'):
+        await message.channel.send("""Type ?pack *amount* on the #packing chanell to ask for a pack.
+If someone asks for a pack, click :up: reaction to notify him 
+that you posted pack on gm.""")
+
+client.run(TOKEN)
